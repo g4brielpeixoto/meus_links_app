ef900fccc4059166689359a9b953d664bd9fc06f
g4briel.soarespeixoto/teksystem <g4briel.soarespeixoto@gmail.com>
2022-03-07 17:19:19 -0300

    Criação do método para excluir um ítem da tela

--- components/organisms/LinkEditor.vue
+++ components/organisms/LinkEditor.vue
@@ -3,16 +3,18 @@
   <div class="link-editor">
     <img class="handle" src="@/assets/images/three-dots-vertical.svg">
     <div class="inputs">
-      <input  v-model="text" class="input text" name="Texto" type="text" placeholder="Texto"/>
-      <input v-model="link" class="input link" name="Link" type="text" placeholder="Link"/>
+      <input  v-model="title" class="input text" name="Texto" type="text" placeholder="Texto"/>
+      <input v-model="url" class="input link" name="Link" type="text" placeholder="Link"/>
     </div>
     <div class="actions">
-      <Switcher  :state="state" @toggleState="state = !state"/>
-      <svg class="trash" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 30 30" width="30px" height="30px">
-        <g id="surface75143999">
-          <path style="stroke:none;fill-rule:nonzero;" d="M 13 3 C 12.734375 2.996094 12.472656 3.101562 12.285156 3.289062 C 12.09375 3.476562 11.988281 3.730469 11.984375 4 L 6 4 C 5.640625 3.996094 5.304688 4.183594 5.121094 4.496094 C 4.941406 4.808594 4.941406 5.191406 5.121094 5.503906 C 5.304688 5.816406 5.640625 6.003906 6 6 L 24 6 C 24.359375 6.003906 24.695312 5.816406 24.878906 5.503906 C 25.058594 5.191406 25.058594 4.808594 24.878906 4.496094 C 24.695312 4.183594 24.359375 3.996094 24 4 L 18.015625 4 C 18.011719 3.730469 17.90625 3.476562 17.714844 3.289062 C 17.527344 3.101562 17.265625 2.996094 17 3 Z M 6 8 L 6 24 C 6 25.105469 6.894531 26 8 26 L 22 26 C 23.105469 26 24 25.105469 24 24 L 24 8 Z M 6 8 " fill="#7a7a7a"/>
-        </g>
-      </svg>
+      <Switcher :state="active" @toggleState="active = !active"/>
+      <button class="button-trash" @click="$emit('delete')">
+        <svg class="trash" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 30 30" width="30px" height="30px">
+          <g id="surface75143999">
+            <path style="stroke:none;fill-rule:nonzero;" d="M 13 3 C 12.734375 2.996094 12.472656 3.101562 12.285156 3.289062 C 12.09375 3.476562 11.988281 3.730469 11.984375 4 L 6 4 C 5.640625 3.996094 5.304688 4.183594 5.121094 4.496094 C 4.941406 4.808594 4.941406 5.191406 5.121094 5.503906 C 5.304688 5.816406 5.640625 6.003906 6 6 L 24 6 C 24.359375 6.003906 24.695312 5.816406 24.878906 5.503906 C 25.058594 5.191406 25.058594 4.808594 24.878906 4.496094 C 24.695312 4.183594 24.359375 3.996094 24 4 L 18.015625 4 C 18.011719 3.730469 17.90625 3.476562 17.714844 3.289062 C 17.527344 3.101562 17.265625 2.996094 17 3 Z M 6 8 L 6 24 C 6 25.105469 6.894531 26 8 26 L 22 26 C 23.105469 26 24 25.105469 24 24 L 24 8 Z M 6 8 " fill="#7a7a7a"/>
+          </g>
+        </svg>
+      </button>
 
     </div>
   </div>
@@ -23,15 +25,15 @@ import Vue from 'vue'
 
 export default Vue.extend({
   props: {
-    text: {
+    title: {
       type: String,
       required: true
     },
-    link: {
+    url: {
       type: String,
       required: true
     },
-    state: {
+    active: {
       type: Boolean,
       required: true
     },
@@ -84,15 +86,18 @@ export default Vue.extend({
     grid-gap: 0.5rem;
     align-items: center;
     justify-items: center;
-    .trash {
-      width: 1.2rem;
-      cursor: pointer;
-      path {
-        transition: 0.3s;
+    .button-trash {
+      background: $lightGray;
+      .trash {
+        width: 1.2rem;
+        cursor: pointer;
+        path {
+          transition: 0.3s;
+        }
       }
-    }
-    .trash:hover path{
-      fill: $pink;
+      .trash:hover path{
+        fill: red;
+      }
     }
   }
 }
