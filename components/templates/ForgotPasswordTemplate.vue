f35c83ceb0c3b5d0221efaa1bd964fa6852bacd1
g4briel.soarespeixoto/teksystem <g4briel.soarespeixoto@gmail.com>
2022-03-08 11:11:41 -0300

    Criação da tela de recuperação de senha.

---
+++ components/templates/ForgotPasswordTemplate.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="login-template">
+    <Header>
+      <Logo />
+    </Header>
+    <Container>
+      <Title text="Qual o seu e-mail cadastrado?" />
+      <form class="form" @submit.prevent="register()">
+        <BaseInput  name="email" type="email" placeholder="E-mail" @changeValue="changeEmail" />
+        <MainButton type="submit" text="RECUPERAR SENHA" />
+      </form>
+    </Container>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  data() {
+    return {
+      username: '',
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    changeUsername(username: string) {
+      this.username = username
+    },
+    changeEmail(email: string) {
+      this.email = email
+    },
+    changePassword(password: string) {
+      this.password = password
+    },
+    async forgotPassword() {
+      const user = {
+        username: this.username,
+        email: this.email,
+        password: this.password,
+        redirectUrl: 'http://localhost:3000/confirm',
+      }
+
+      await this.$store.dispatch('register', user)
+      this.$notify({
+        type: 'success',
+        text: 'Bem-vindo ao Meus Links! Verifique seu email para ativar sua conta!',
+        duration: 10000
+      })
+    }
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+.logo {
+  width: 9.3rem;
+}
+.header {
+  justify-content: start;
+}
+.container {
+  grid-gap: 1.5rem;
+}
+.form {
+  display: grid;
+  grid-gap: 1rem;
+}
+.main-button {
+  margin: 1rem auto;
+}
+</style>
