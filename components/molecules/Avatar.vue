21dd86b7561e902508d9a0a5f3701a0392717c5c
g4briel.soarespeixoto/teksystem <g4briel.soarespeixoto@gmail.com>
2022-03-14 17:31:47 -0300

    Criação do componente de Avatar

---
+++ components/molecules/Avatar.vue
@@ -0,0 +1,27 @@
+<template>
+  <img src="@/assets/images/avatar.png" :alt="name" class="avatar">
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  props: {
+    src: {
+      type: String,
+      default: '@/assets/images/avatar.png'
+    },
+    name: {
+      type: String,
+      default: 'Foto de perfil'
+    }
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+.avatar {
+  width: 8rem;
+  border-radius: 5rem;
+}
+</style>
