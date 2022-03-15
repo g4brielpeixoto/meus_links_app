9ddaf2866d12739dad01a46b6942f03e43831c6f
g4briel.soarespeixoto/teksystem <g4briel.soarespeixoto@gmail.com>
2022-03-15 11:13:00 -0300

    Criação do componente avatar

--- components/molecules/Avatar.vue
+++ components/molecules/Avatar.vue
@@ -1,5 +1,16 @@
 <template>
-  <img src="@/assets/images/avatar.png" :alt="name" class="avatar">
+  <div class="box" @mouseenter="show = true" @mouseleave="show = false">
+    <transition>
+      <div v-if="enableChange && show" class="change-avatar" >
+        <div>
+          <label for="avatar" class="actions update-avatar">Trocar foto</label>
+          <input id="avatar" ref="file" type="file" @input="$emit('submited', $event)">
+        </div>
+        <p class="actions delete-avatar" @click="$emit('delete')">Deletar foto</p>
+      </div>
+    </transition>
+    <img :src="src" :alt="name" class="avatar">
+  </div>
 </template>
 
 <script lang="ts">
@@ -9,11 +20,20 @@ export default Vue.extend({
   props: {
     src: {
       type: String,
-      default: '@/assets/images/avatar.png'
+      required: true
     },
     name: {
       type: String,
       default: 'Foto de perfil'
+    },
+    enableChange: {
+      type: Boolean,
+      required: true
+    }
+  },
+  data() {
+    return {
+      show: false
     }
   }
 })
@@ -23,5 +43,38 @@ export default Vue.extend({
 .avatar {
   width: 8rem;
   border-radius: 5rem;
+}
+
+.change-avatar {
+  background: rgba(255, 94, 111, 0.85);
+  position: absolute;
+  width: 8rem;
+  height: 8rem;
+  border-radius: 5rem;
+  color: $white;
+  display: grid;
+  justify-items: center;
+  align-items: center;
+  align-content: center;
+  grid-gap: 0.7rem;
+  input {
+    display: none;
+  }
+  .actions {
+    cursor: pointer;
+    &:hover {
+      text-decoration: underline;
+    } 
+  }
+}
+
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
 }
 </style>
