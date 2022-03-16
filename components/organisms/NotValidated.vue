15fab5bcff5ff5ecb9c6bf5c279908bd963267b5
g4briel.soarespeixoto/teksystem <g4briel.soarespeixoto@gmail.com>
2022-03-16 10:11:41 -0300

    Componente que será exibido na página se o dono não estiver validado

---
+++ components/organisms/NotValidated.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="not-validated">
+    <Container>
+      <Logo />
+      <Title text="Usuário não verificado."/>
+      <SubTitle
+        :text="`@${username} ainda não validou seu cadastro, 
+        para fazer isso basta clicar no link enviado por e-mail.`"/>
+    </Container>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  props: {
+    username: {
+      type: String,
+      required: true
+    }
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+.not-validated {
+  margin-top: 4rem;
+  display: grid;
+
+  .logo {
+    width: 12rem;
+    margin-bottom: 2rem;
+  }
+
+  .title {
+    margin-bottom: 0.5rem;
+  }
+
+  .subtitle {
+    width: 25rem;
+  }
+}
+</style>
