52e89cdad878111a8ac80354cf78f2d950f196e9
bilpeixoto <g4briel.soarespeixoto@gmail.com>
2022-03-21 10:42:59 -0300

    Remoção da mudança de rota no vuex

--- store/index.ts
+++ store/index.ts
@@ -32,7 +32,6 @@ export const actions: ActionTree<RootState, RootState> = {
         username: payload.username, 
         password: payload.password
       })
-      this.$router.push('/admin')
     }
   },
 
