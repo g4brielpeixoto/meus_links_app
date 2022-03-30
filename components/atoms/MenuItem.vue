5c5b655beaa6701d81ee0bb2401195596c6f67e6
g4briel.soarespeixoto/teksystem <g4briel.soarespeixoto@gmail.com>
2022-03-30 17:16:07 -0300

    Criação de um componente para o item do menu hamburguer.

---
+++ components/atoms/MenuItem.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="menu-item" @click="$emit('click')">
+    <ButtonLoading v-show="loading"/>
+    <div :class="[{ 'loading': loading }, 'item']">
+      <DotNotification  v-show="highlight" />
+      <img :src="require(`@/assets/images/${icon}`)" class="icon">
+      <NuxtLink v-if="to" :to="to">{{ text }}</NuxtLink>
+      <a v-else>{{ text }}</a>
+    </div>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+import DotNotification from './DotNotification.vue'
+
+export default Vue.extend({
+  components: { DotNotification },
+  props: {
+    to: {
+      type: String,
+      default: ''
+    },
+    loading: {
+      type: Boolean,
+      default: false
+    },
+    highlight: {
+      type: Boolean,
+      default: false
+    },
+    text: {
+      type: String,
+      required: true
+    },
+    icon: {
+      type: String,
+      required: true
+    }
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+.menu-item {
+  display: grid;
+  grid-auto-flow: column;
+  grid-gap: 1rem;
+
+  .button-loading {
+    margin-left: 60px;
+  }
+  .icon {
+    margin-top: 2px;
+    width: 20px;
+  }
+  .item {
+    cursor: pointer;
+  }
+  .dot-notification {
+    margin-top: 7px;
+  }
+
+  .loading {
+    cursor: progress;
+    opacity: 0;
+  }
+}
+</style>
