2cc9dd1ff48556756d25e3962d7275ae1ec9bdc4
g4briel.soarespeixoto/teksystem <g4briel.soarespeixoto@gmail.com>
2022-03-30 17:19:05 -0300

    Criação de um componente para notificação de alterações não salvas.

---
+++ components/atoms/DotNotification.vue
@@ -0,0 +1,16 @@
+<template>
+  <transition>
+    <span class="dot-notification"></span>
+  </transition>
+</template>
+ 
+<style lang="scss" scoped>
+.dot-notification {
+  height: 10px;
+  width: 10px;
+  border-radius: 50%;
+  display: inline-block;
+  border-radius: 50%;
+  background-color: $pink;
+}
+</style>
