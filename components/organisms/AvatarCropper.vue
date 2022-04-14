87fcc1e185fe19711c390c1311259660aff4ac12
gabrielpeixoto/teksystem <gabrielpeixoto@teksystem.com.br>
2022-04-14 17:29:13 -0300

    Componente avatar cropper

---
+++ components/organisms/AvatarCropper.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="avatar-cropper">
+    <div class="avatar-cropper__container">
+      <div class="avatar-cropper__container__image">
+        <img :src="image" />
+      </div>
+      <div class="avatar-cropper__container__cropper">
+        <div class="avatar-cropper__container__cropper__canvas">
+          <canvas ref="canvas" />
+        </div>
+        <div class="avatar-cropper__container__cropper__buttons">
+          <button class="avatar-cropper__container__cropper__buttons__button" @click="crop">Crop</button>
+          <button class="avatar-cropper__container__cropper__buttons__button" @click="cancel">Cancel</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  
+})
+</script>
+
+<style>
+
+</style>
